661963a7fdc94debdab9f44393f381361e338284
hilburn <charliepaterson89@gmail.com>
2015-01-03 16:59:17 +0000

    Improved setDonationURL

--- src/main/java/hilburnlib/utils/RegisterDonation.java
+++ src/main/java/hilburnlib/utils/RegisterDonation.java
@@ -4,17 +4,19 @@ import java.lang.reflect.Method;
 
 public class RegisterDonation
 {
-    public static void setDonationURL(String ID, String donationUrl)
+    public static void setDonationURL(String modID, String donationUrl)
     {
         try
         {
             Class donationManager = Class.forName("openblocks.common.DonationURLManager");
             Method instance = donationManager.getMethod("instance");
             Method url = donationManager.getMethod("addUrl");
-            url.invoke(instance.invoke(null),ID,donationUrl);
+            url.invoke(instance.invoke(null),modID,donationUrl);
+            LogHelper.info("Donation Station Integration Completion");
         } catch (ClassNotFoundException e)
         {
-            //OpenBlocks isn't loaded
+            //OpenBlocks isn't loaded - not actually an error
+            LogHelper.info("Donation Station Perambulation, Location Calibration Frustration");
         } catch (Exception e)
         {
             LogHelper.warn("Donation Station Integration Frustration");
