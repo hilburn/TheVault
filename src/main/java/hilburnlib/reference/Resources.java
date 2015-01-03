a9cf12eb3e0c3514708032a3ef65a72114d25591
hilburn <charliepaterson89@gmail.com>
2015-01-03 13:13:54 +0000

    NER methods

---
+++ src/main/java/hilburnlib/reference/Resources.java
@@ -0,0 +1,19 @@
+package hilburnlib.reference;
+
+import net.minecraft.util.ResourceLocation;
+
+public final class Resources
+{
+    public static final class Gui
+    {
+        public static final class Nei
+        {
+
+        }
+    }
+
+    public static final class Vanilla
+    {
+        public static final ResourceLocation FONT = new ResourceLocation("textures/font/ascii.png");
+    }
+}
