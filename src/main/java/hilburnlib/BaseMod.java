525ffd20726fef69d612ff9f07389d716f6598c4
hilburn <charliepaterson89@gmail.com>
2015-01-03 12:59:53 +0000

    Generalizations are fun

--- src/main/java/hilburnlib/BaseMod.java
+++ src/main/java/hilburnlib/BaseMod.java
@@ -5,13 +5,8 @@ import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import hilburnlib.items.ItemUtils;
 import hilburnlib.reference.Metadata;
 import hilburnlib.reference.Reference;
-import hilburnlib.utils.RegisterDonation;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 
 @Mod(modid = Reference.ID, name = Reference.NAME, version = Reference.VERSION_FULL)
 public class BaseMod
@@ -25,7 +20,6 @@ public class BaseMod
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
         metadata = Metadata.init(metadata);
-        RegisterDonation.setDonationURL(Reference.ID,"blah");
     }
 
     @Mod.EventHandler
---
+++ src/main/java/hilburnlib/reflection/ClassScraper.java
@@ -0,0 +1,31 @@
+package hilburnlib.reflection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClassScraper
+{
+    public static Set<Class> getGeneralizations(Class classObject)
+    {
+        Set<Class> generalizations = new HashSet<Class>();
+        generalizations.add(classObject);
+        Class superClass = classObject.getSuperclass();
+        if (superClass != null)
+        {
+            generalizations.addAll(getGeneralizations(superClass));
+        }
+        Class[] superInterfaces = classObject.getInterfaces();
+        for (int i = 0; i < superInterfaces.length; i++)
+        {
+            Class superInterface = superInterfaces[i];
+            generalizations.addAll(getGeneralizations(superInterface));
+        }
+        return generalizations;
+    }
+
+
+    public static boolean classInstanceOf(Class clazz, Class instance)
+    {
+        return getGeneralizations(clazz).contains(instance);
+    }
+}
