5ec151b1015c69cc7dc0d2b63fe3f4798aa0f022
hilburn <charliepaterson89@gmail.com>
2015-01-05 13:30:09 +0000

    Generalized Sync

--- src/main/java/hilburnlib/config/Sync.java
+++ src/main/java/hilburnlib/config/Sync.java
@@ -9,12 +9,13 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
-public class Config
+public class Sync
 {
-    public static byte[] getConfigMessage()
+    public static byte[] getSyncMessage(Class clazz)
     {
-        Field[] fields = Config.class.getFields();
+        Field[] fields = clazz.getFields();
         NBTTagCompound nbtConfig = new NBTTagCompound();
+        nbtConfig.setString("C", clazz.getName());
         nbtConfig.setShort("L", (short) fields.length);
         for (short i=0;i<fields.length;i++)
         {
@@ -39,12 +40,12 @@ public class Config
         return bytes;
     }
 
-    public static void loadConfigMessage(byte[] bytes)
+    public static void loadSyncMessage(byte[] bytes)
     {
         try
         {
             NBTTagCompound config = CompressedStreamTools.func_152457_a(bytes,new NBTSizeTracker(bytes.length*8));
-            Field[] fields = Config.class.getFields();
+            Field[] fields = Class.forName(config.getString("C")).getFields();
             short length = config.getShort("L");
             for (short i = 0; i<length; i++)
             {
@@ -52,11 +53,7 @@ public class Config
                 if (obj!=null)
                     fields[i].set(null,obj);
             }
-        } catch (IOException e)
-        {
-        } catch (IllegalAccessException e)
-        {
-        }
+        } catch (Exception e){}
         return;
     }
 }
