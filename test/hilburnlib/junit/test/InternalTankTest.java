a076a920243429bb528a0f1e7602a7ffe70518fb
hilburn <charliepaterson89@gmail.com>
2015-01-09 10:51:33 +0000

    InternalTankTest

---
+++ test/hilburnlib/junit/test/InternalTankTest.java
@@ -0,0 +1,95 @@
+package hilburnlib.junit.test;
+
+import hilburnlib.base.tank.InternalTank;
+import hilburnlib.junit.MCTestRunner;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(MCTestRunner.class)
+public class InternalTankTest
+{
+
+    InternalTank tank;
+
+    public InternalTankTest()
+    {
+        //TODO getting a NPE from the FluidRegistry
+        tank = new InternalTank(new FluidStack(FluidRegistry.WATER,2000),3000);
+    }
+
+    @Test
+    public void Tanktest() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetFluid() throws Exception
+    {
+        assertTrue(tank.getFluid()!=null);
+        assertTrue(tank.getFluid().isFluidStackIdentical(new FluidStack(FluidRegistry.WATER,2000)));
+    }
+
+    @Test
+    public void testGetFluidAmount() throws Exception
+    {
+        assertTrue(tank.getFluidAmount()>0);
+    }
+
+    @Test
+    public void testNBTReadWrite() throws Exception
+    {
+        NBTTagCompound tagCompound = tank.writeToNBT(new NBTTagCompound());
+        InternalTank newTank = new InternalTank(0).readFromNBT(tagCompound);
+        assertEquals(tank.getCapacity(),newTank.getCapacity());
+        assertTrue((tank.getFluid()==null && newTank.getFluid()==null) || tank.getFluid().isFluidStackIdentical(newTank.getFluid()));
+    }
+
+    @Test
+    public void testIsValidFluid() throws Exception
+    {
+        assertTrue(tank.isValidFluid(FluidRegistry.WATER));
+        assertFalse(tank.isValidFluid(new FluidStack(FluidRegistry.LAVA,1)));
+    }
+
+    @Test
+    public void testGetInfo() throws Exception
+    {
+        assertEquals(tank.getInfo().capacity,tank.getCapacity());
+        assertTrue(tank.getInfo().fluid.isFluidStackIdentical(tank.getFluid()));
+    }
+
+    @Test
+    public void testFill() throws Exception
+    {
+        assertTrue(tank.fill(new FluidStack(FluidRegistry.LAVA,1),true)==0);
+        assertTrue(tank.fill(new FluidStack(FluidRegistry.WATER,500),false)==500);
+        assertTrue(tank.fill(new FluidStack(FluidRegistry.WATER,1500),true)==1000);
+        assertTrue(tank.getCapacity()==tank.getFluidAmount());
+    }
+
+    @Test
+    public void testDrain() throws Exception
+    {
+        assertTrue(tank.drain(500,true).amount==500);
+    }
+
+    @Test
+    public void testSetFluid() throws Exception
+    {
+        tank.setFluid(new FluidStack(FluidRegistry.WATER,50));
+        assertTrue(tank.getFluidAmount()==50);
+    }
+
+    @Test
+    public void testSetCapacity() throws Exception
+    {
+        tank.setCapacity(5000);
+        assertTrue(tank.getCapacity()==5000);
+    }
+}
