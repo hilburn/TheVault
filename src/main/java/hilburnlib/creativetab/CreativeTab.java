78263ae2dad1fd8e3d9be011587a40c99a57c0ba
way2muchnoise <jens.baetens@live.be>
2015-02-05 20:44:52 +0100

    improves the CreativeTab

--- src/main/java/hilburnlib/creativetab/CreativeTab.java
+++ src/main/java/hilburnlib/creativetab/CreativeTab.java
@@ -2,6 +2,7 @@ package hilburnlib.creativetab;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,7 +10,7 @@ import net.minecraft.item.ItemStack;
 public class CreativeTab extends CreativeTabs
 {
     private ItemStack iconItemStack;
-    
+
     public CreativeTab(String label, Item iconItem)
     {
         this(label, new ItemStack(iconItem));
@@ -20,10 +21,45 @@ public class CreativeTab extends CreativeTabs
         this(label, new ItemStack(iconItem, 1, meta));
     }
 
+    public CreativeTab(String label, Block iconBlock)
+    {
+        this(label, new ItemStack(iconBlock));
+    }
+
+    public CreativeTab(String label, Block iconBlock, int meta)
+    {
+        this(label, new ItemStack(iconBlock, 1, meta));
+    }
+
+    public void setIcon(Item iconItem)
+    {
+        this.iconItemStack = new ItemStack(iconItem);
+    }
+
+    public void setIcon(Item iconItem, int meta)
+    {
+        this.iconItemStack = new ItemStack(iconItem, 1, meta);
+    }
+
+    public void setIcon(Block iconBlock)
+    {
+        this.iconItemStack = new ItemStack(iconBlock);
+    }
+
+    public void setIcon(Block iconBlock, int meta)
+    {
+        this.iconItemStack = new ItemStack(iconBlock, 1, meta);
+    }
+
     public CreativeTab(String label, ItemStack iconItemStack)
     {
         super(label);
         this.iconItemStack = iconItemStack;
+    }
+
+    public CreativeTab(String label)
+    {
+        super(label);
     }
 
     @SideOnly(Side.CLIENT)
