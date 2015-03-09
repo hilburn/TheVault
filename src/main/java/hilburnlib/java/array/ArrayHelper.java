f1f2addfd1af04d748b377bcddf0dbeeffa0c765
way2muchnoise <jens.baetens@live.be>
2015-03-09 21:13:49 +0100

    adds some usefull methods to StringHelper and ArrayHelper

--- src/main/java/hilburnlib/java/array/ArrayHelper.java
+++ src/main/java/hilburnlib/java/array/ArrayHelper.java
@@ -2,10 +2,60 @@ package hilburnlib.java.array;
 
 import hilburnlib.java.math.MathHelper;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class ArrayHelper
 {
+    /**
+     * Converts an {@link java.lang.Object} to an array of the type
+     * @param array the {@link java.lang.Object} representation of the array
+     * @return an {@link java.lang.Object}[]
+     */
+    public static Object[] convertToArray(Object array) {
+        Class ofArray = array.getClass().getComponentType();
+        if (ofArray.isPrimitive())
+        {
+            List arrayList = new ArrayList();
+            int length = Array.getLength(array);
+            for (int i = 0; i < length; i++)
+            {
+                arrayList.add(Array.get(array, i));
+            }
+            return arrayList.toArray();
+        }
+        else
+        {
+            return (Object[]) array;
+        }
+    }
+
+    /**
+     * Clear all null values from an array
+     *
+     * @param array the array
+     * @param type  the {@link java.lang.Class} of the array
+     * @param <T>   the type of the array as state above
+     * @return a null less array
+     */
+    @SuppressWarnings("SuspiciousMethodCalls")
+    public static <T> T[] removeNulls(T[] array, Class<T> type)
+    {
+        List<T> list = new LinkedList<T>();
+        for (T value : array)
+        {
+            if (value != null)
+            {
+                list.add(value);
+            }
+        }
+        return list.toArray((T[]) Array.newInstance(type, list.size()));
+    }
+
     /**
      * Takes a sub array from element start to element end (end is excluded)
      *
