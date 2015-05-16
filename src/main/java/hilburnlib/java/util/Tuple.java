431cadc17ae0da56ab41c65834951ab6d7521141
way2muchnoise <jens.baetens@live.be>
2015-05-16 19:25:04 +0200

    comment on the Tuple class

--- src/main/java/hilburnlib/java/util/Tuple.java
+++ src/main/java/hilburnlib/java/util/Tuple.java
@@ -2,6 +2,13 @@ package hilburnlib.java.util;
 
 import java.util.Map;
 
+/**
+ * Storage object for 2 sub-objects
+ * Can be constructed from a {@link java.util.Map.Entry}
+ * Basically a better implementation of {@link net.minecraft.util.Tuple}
+ * @param <V> type of the first object
+ * @param <W> type of the second object
+ */
 public class Tuple<V, W>
 {
     private V objV;
@@ -18,21 +25,37 @@ public class Tuple<V, W>
         this(entry.getKey(), entry.getValue());
     }
 
+    /**
+     * Set the first object
+     * @param obj the object of type {@link V}
+     */
     public void setFirst(V obj)
     {
         this.objV = obj;
     }
 
+    /**
+     * Set the second object
+     * @param obj the object of type {@link W}
+     */
     public void setSecond(W obj)
     {
         this.objW = obj;
     }
 
+    /**
+     * Get the first object
+     * @return an object of type {@link V}
+     */
     public V getFirst()
     {
         return objV;
     }
 
+    /**
+     * Get the first object
+     * @return an object of type {@link W}
+     */
     public W getSecond()
     {
         return objW;
